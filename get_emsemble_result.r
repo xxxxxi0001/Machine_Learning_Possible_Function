737877e1828d2abf3db96e82f048dc3169bc7544
XXX <zhang.xi6@northeastern.edu>
2025-11-08 13:46:35 -0500

    Refactor get_emsemble_result function parameters
    Removed num_ensemble parameter and updated loop to iterate over logistic_model_list.

--- get_emsemble_result.r
+++ get_emsemble_result.r
@@ -1,14 +1,13 @@
-# Input: 1). num_ensemble: the number of ensemble you create
-#        2). logistic_model_list: the logistic regression model you create with your ensemble list
-#        3). df: the data frame you use to make prediction
-#        4). index: the index you wanna try with this model (usually test & val index)
-#        5). weight_list: the weight you get for each ensemble
+# Input: 1). logistic_model_list: the logistic regression model you create with your ensemble list
+#        2). df: the data frame you use to make prediction
+#        3). index: the index you wanna try with this model (usually test & val index)
+#        4). weight_list: the weight you get for each ensemble
 # Return:1). ensemble_predictions: list of prediction (in probability) made 
 #            with ensemble Logistic Regression model
-get_emsemble_result<-function(num_ensemble,logistic_model_list,df,index,weight_list) {
+get_emsemble_result<-function(logistic_model_list,df,index,weight_list) {
    prediction_val<-list()
 
-  for (i in 1:num_ensemble) {
+  for (i in 1:length(logistic_model_list)) {
     prediction_val[[i]]<-predict(logistic_model_list[[i]], df[index,], type="response")
     prediction_val[[i]]<-prediction_val[[i]]*weight_list[[i]]
   }
