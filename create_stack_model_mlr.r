8bf3b313e347dd5f1ef4deb93837fd5711e1c7d6
XXX <zhang.xi6@northeastern.edu>
2025-11-09 00:43:50 -0500

    Create create_stack_model_mlr.r file

--- create_stack_model_mlr.r
+++ create_stack_model_mlr.r
@@ -4,7 +4,7 @@
 #        4). model_list: the list of ensemble model you made ealier
 # Return:1). stack_model: stacked model you generate from ensemble models, which 
 #            automatically gives how each model should contribute to the overall prediction
-create_stack_model_lm<-function (df,target_col,test_index,model_list) {
+create_stack_model_mlr<-function (df,target_col,test_index,model_list) {
   stack_df<-data.frame(true_value=df[[target_col]][test_index])
   for (i in 1:length(model_list)) {
     stack_df[[paste0("model",i)]]<-predict(model_list[[i]],df[test_index,])
