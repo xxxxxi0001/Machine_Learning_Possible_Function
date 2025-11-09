4053ba4c0389eed7d2ee6b4f4c5c32219011e061
XXX <zhang.xi6@northeastern.edu>
2025-11-09 12:29:21 -0500

    Refactor check_na_zero function for NA and zero counts
    Refactor check_na_zero function to improve readability and maintainability.

--- check_na.r
+++ check_na.r
@@ -1,17 +1,24 @@
+```{r}
 # Input: 1). data frame
 # Return: nothing, but will show if data frame has zero/NA value
 check_na_zero<-function(df){
+  
+  # Ignore non-numerical column
   for (i in 1:ncol(df)) {
   if (is.numeric(df[[i]])){
-      na_count <-sum(is.na(df[[i]]))
-      if (na_count>0) {
+    
+    # Count NA, if appear, show which column has NA
+    na_count <-sum(is.na(df[[i]]))
+    if (na_count>0) {
       cat("There are total", na_count ,"NA value in column", colnames(df)[i],"\n")
       }
-      
-      zero_count<-length(which(df[[i]] == 0))
-      if (zero_count>0) {
+    
+    # Count zero value, if appear, show which column has zero value
+    zero_count<-length(which(df[[i]] == 0))
+    if (zero_count>0) {
       cat("There are total", zero_count, "Zero value in column", colnames(df)[i], "\n")
       }
     }
   }
 }
+```
