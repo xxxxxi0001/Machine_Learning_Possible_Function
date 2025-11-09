93b26f6a12af6d1b2ec9c45d6da5522442953883
XXX <zhang.xi6@northeastern.edu>
2025-11-09 12:33:41 -0500

    Refactor replace_na_with_zero function
    Updated function to improve clarity and maintainability.

--- replace_zero_with_na.r
+++ replace_zero_with_na.r
@@ -2,10 +2,14 @@
 #        2). ignore_cols: zero value that does not need to be changed into NA
 # Return:1). df: data frame with designated columns' zero value replaced into na
 replace_na_with_zero<-function(df,ignore_cols) {
+  
+  # For only column that their zero value need to change into NA
   for (i in 1:ncol(df)) {
   if (is.numeric(df[[i]]) && !(colnames(df)[i] %in% ignore_cols)){
-      zero_count<-length(which(df[[i]] == 0))
-      if (zero_count>0) {
+     
+    # If their zero value is above 0, return result & change them into NA
+    zero_count<-length(which(df[[i]] == 0))
+    if (zero_count>0) {
       cat("Total", zero_count, "Zero value in column", colnames(df)[i],"has successfully changed into NA","\n")
       df[[i]][df[[i]]==0]<-NA
       }
