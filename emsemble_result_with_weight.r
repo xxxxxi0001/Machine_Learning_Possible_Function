da2fe9cd4d6fea7b350c94c876ab275083ad4b24
XXX <zhang.xi6@northeastern.edu>
2025-11-09 13:48:11 -0500

    Enhance emsemble_result_with_weight with target_treatment
    Added target_treatment parameter to emsemble_result_with_weight function for handling transformations.

--- emsemble_result_with_weight.r
+++ emsemble_result_with_weight.r
@@ -1,10 +1,32 @@
+# Input: 1). x: the variable that's been or not been transformed
+#        2). treatment: could be "log","log1p","sqrt","square","none"
+# Return:1). Transform back original value
+reverse_num<-function(x,treatment) {
+  if (tolower(treatment)=="log") {
+    return(exp(x))
+    }
+  if (tolower(treatment)=="log1p") {
+    return(exp(x)-1)
+    }
+  if (tolower(treatment)=="sqrt") {
+    return(x^2)
+    }
+  if (tolower(treatment)=="square") {
+    return(sqrt(x))
+    }
+  if (tolower(treatment)=="none") {
+    return(x)
+    }
+}
+
 # Input: 1). model_list: the logistic regression model you create with your ensemble list
 #        2). df: the data frame you use to make prediction
 #        3). index: the index you wanna try with this model (usually test & val index)
 #        4). weight_list: the weight you get for each ensemble
+#        5). target_treatment: if no transformation occur, it is automatically none, but can also be "log","log1p","sqrt","square"
 # Return:1). ensemble_predictions: list of prediction (in probability) made 
 #            with ensemble Logistic Regression model
-emsemble_result_with_weight<-function(model_list,df,index,weight_list) {
+emsemble_result_with_weight<-function(model_list,df,index,weight_list,target_treatment="none") {
   
   # Initialization
   prediction_val<-list()
@@ -16,5 +38,7 @@ emsemble_result_with_weight<-function(model_list,df,index,weight_list) {
   
   # Calculate ensemble model's mean as final prediciton value
   ensemble_predictions<-Reduce("+",prediction_val)
+  ensemble_predictions<-reverse_num(ensemble_predictions,target_treatment)
+  
   return(ensemble_predictions)
 }
