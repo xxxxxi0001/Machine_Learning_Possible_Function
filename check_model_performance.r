0f23d0f08b8fca96b00b02ca06718ac102368438
XXX <zhang.xi6@northeastern.edu>
2025-11-09 13:11:20 -0500

    Fix formatting and improve check_model_performance function

--- check_model_performance.r
+++ check_model_performance.r
@@ -1,3 +1,4 @@
+```{r}
 # Input: 1). predict_prob: the prediction result (in probability/response)
 #        2). threshold: above what number is positive
 #        3). positive: target's positive value 
@@ -6,23 +7,30 @@
 #        6). test_index: the index you select for test
 #        7). target_col: your target name 
 # Return:No return but will automatically give you how accurate your model is with accuracy, tpr, tnr, F1 value
-check_model_performance<-function (predict_prob,threshold,positive,negative,df,test_index,target_col) {
+check_model_performance<-function (predict_prob, threshold, positive, negative, df, test_index, target_col) {
   
+  # First get prediciton & real value
   prediction<-ifelse(predict_prob>=threshold,positive,negative)
   true_value<-df[[target_col]][test_index]
   
+  # Calculate their tn, tp, fp, fn
   TN<-sum(prediction==negative&true_value==negative)  
   TP<-sum(prediction==positive&true_value==positive)
   FP<-sum(prediction==positive&true_value==negative)
   FN<-sum(prediction==negative&true_value==positive)
   
+  # Calculate their precision & recall for F1
+  # If denominator is zero, call zero as result to avoid NA
   precision<-ifelse(TP+FP==0, 0, TP/(TP+FP))
   recall<-ifelse(TP+FN==0, 0, TP/(TP+FN))
-  
   F1<-ifelse(precision+recall==0, 0, round(2*precision*recall/(precision+recall),2))
+  
+  # Calculate accuracy, tpr, tnr
   accuracy<-round(((TP+TN)/(TP+TN+FP+FN))*100,2)
   tpr<-round((TP/(TP+FN))*100,2)
   tnr<-round((TN/(TN+FP))*100,2)
-
+  
+  # Print out result
   cat("The accuracy for is", accuracy, "%. The True Positive Rate is ", tpr, "%. And the True Negative Rate is ", tnr, "% and the F1 score is", F1,".")
 }
+```
