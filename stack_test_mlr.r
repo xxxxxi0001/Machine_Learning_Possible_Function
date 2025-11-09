ff37c3c3f6d100e3ad2021cd79372c0ef59c6578
XXX <zhang.xi6@northeastern.edu>
2025-11-09 14:18:32 -0500

    Modify target_treatment options and reverse_num logic
    Updated target_treatment options and added conditional reverse_num call.

--- stack_test_mlr.r
+++ stack_test_mlr.r
@@ -25,29 +25,39 @@ reverse_num<-function(x,treatment) {
 #        4). validation_index: index used for validation purpose
 #        5). target_col: the feature you want to make prediction with
 #        6). target_treatment: if target has been transformed, what type? 
-#            accept "log","sqrt","square","none"
+#            accept "log","log1p","sqrt","square","none"
 # Return:1). prediction_value: stack model's predict value
 #        2). true_value: the actual value 
 # Print: 1). rmse: error between true value and prediction value
 stack_test_mlr<-function (stack_model,model_list,df,validation_index,target_col,target_treatment) {
   
+  # Create an empty data frame with length of validation index
   stack_df_val<-data.frame(matrix(nrow=length(validation_index),ncol=0))
   
+  # Input prediction value of each ensemble model made on validation index
+  # in format of mi<-prediction value
   for (i in 1:length(model_list)) {
     stack_df_val[[paste0("model",i)]]<-predict(model_list[[i]],df[validation_index,])
   }
   
+  # Make prediction with stack model, each ensemble model contribute differently 
+  # to the overall prediction, collect those value
   prediction_value<-predict(stack_model,newdata=stack_df_val)
+  # Get their real value
   true_value<-df[[target_col]][validation_index]
   
-  # Call `reverse_num` function, change them back to their original value
-  prediction_value<-reverse_num(prediction_value,target_treatment)
-  true_value<-reverse_num(true_value,target_treatment)
+  # Call `reverse_num` function if transformation occured
+  # change them back to their original value
+  if (tolower(target_treatment) != "none") {
+    prediction_value<-reverse_num(prediction_value,target_treatment)
+    true_value<-reverse_num(true_value,target_treatment)
+  }
   
+  # Calculate rmse based on prediction & real value & output result
   rmse<-sqrt(mean((true_value-prediction_value)^2))
-  
   cat("The rmse for stack model of these",length(model_list),"models is",rmse,".")
   
+  # Get prediction & true value for more possible testify like plot or cor
   return(list(
     prediction_value=prediction_value,
     true_value=true_value
