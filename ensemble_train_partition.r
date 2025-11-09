4286656b4be257e3dd97e38d94afd70cacf7307f
XXX <zhang.xi6@northeastern.edu>
2025-11-09 12:50:18 -0500

    Fix function syntax and formatting in ensemble_train_partition

--- ensemble_train_partition.r
+++ ensemble_train_partition.r
@@ -5,12 +5,19 @@
 #        5). sub_portion: portion of overall training you want to select as sub-list
 # Return:1). A list contain sub_num number of training index
 ensemble_train_partition<-function(df,seed_num,train_index,sub_num,sub_portion) {
+  
+  # Initialization
   set.seed(seed_num)
   partitions<-list()
+  
+  # Randomly choose designated number (sub_num) of subset from training index 
+  # with designated portion (sub_portion) of number
   for (i in 1:sub_num){
     nt<-length(train_index)
     subset_train_index<-sample(train_index,size=floor(sub_portion*nt))
     partitions[[i]]<-list(train=subset_train_index)
   }
+  
+  # Return a list of partition index
   return(partitions)
 }
